cec72ce524edf8656b48f4c762f9d9f3af82f968
dominggo1999 <arnolddominggo19@gmail.com>
2020-04-26 13:16:47 +0700

    fixed nav hover and added dropdown

--- app.js
+++ app.js
@@ -2,21 +2,27 @@ let navMenu = document.querySelector(".navs");
 let menuBtn = document.querySelector(".menu-btn");
 let width = window.innerWidth;
 
-menuBtn.addEventListener("click",()=>{
-    navMenu.classList.toggle("toggle");
-    menuBtn.classList.toggle("rotate");
-})
-
-if(width >= 720){
-    navMenu.classList.remove("toggle");
-    menuBtn.classList.remove("rotate");
-}
-
-window.addEventListener("resize",()=>{
-    width = window.innerWidth;
+const reset = () =>{
+    menuBtn.addEventListener("click",()=>{
+        navMenu.classList.toggle("toggle");
+        menuBtn.classList.toggle("rotate");
+    })
+    
+    //Hilangkan toggle dan rotate kalau reload 
     if(width >= 720){
         navMenu.classList.remove("toggle");
         menuBtn.classList.remove("rotate");
     }
-})
+    
+    // Hilangkan toggle dan rotate kalau resize
+    window.addEventListener("resize",()=>{
+        width = window.innerWidth;
+        if(width >= 720){
+            navMenu.classList.remove("toggle");
+            menuBtn.classList.remove("rotate");
+        }
+    })
+}
 
+
+reset();
